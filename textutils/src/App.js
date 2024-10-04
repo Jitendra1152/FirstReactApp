d6127481e001e18cf001537c98ffdcaad69c7f01
Jitendra1152 <147709423+Jitendra1152@users.noreply.github.com>
2024-10-04 17:39:51 +0530

    video 17 continue from 7:00

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,17 +1,67 @@
 
+import { useState } from 'react';
 import './App.css';
-import About from './components/About';
+ import About from './components/About';
 import Navbar from './components/Navbar';
-// import TextForm from './components/TextForm';
+ import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+
+
+
 function App() {
+
+  const[mode, setMode] = useState('light'); 
+  const[alert, setAlert] = useState(null);
+
+  const showAlert = (message,type)=>{
+
+    setAlert({
+      msg: message,
+      type: type
+    })
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+
+
+  }
+
+  const toggleMode = ()=> {
+   if(mode === 'light'){
+    setMode('dark');
+    document.body.style.backgroundColor = '#324689';
+    showAlert("Dark mode has been enabled","success");
+   }else{
+     setMode('light');
+     document.body.style.backgroundColor = 'white';
+     showAlert("Light mode has been enabled","success");
+   }    
+  }
   return (
    <>
-  <Navbar title="Textutils" aboutText = "About Textutils"/>
-  <div className="container my-3">
-  {/* <TextForm heading="Enter the text to analyse below"/> */}
-  <About/>
-  </div>
-  
+   <Router>
+    
+   <Navbar title="Textutils" mode={mode} toggleMode={toggleMode} aboutText="About Us"/>
+  <Alert alert={alert}/> 
+   <div className="container my-3"> 
+  <Routes>
+          <Route path="/about" exact element={<About />} />
+        
+          <Route path="/" exact element={<TextForm showAlert={showAlert} heading="Enter the text to analyse below" mode={mode}/> } />
+          
+   </Routes>   
+ </div> 
+
+  </Router>
    </>
   );
 }
--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -5,11 +5,13 @@ export default function TextForm(props) {
     //console.log('Uppercase was clicked ' + text);
     let newtext = text.toUpperCase();
     setText(newtext)
+    props.showAlert("Converted to Upper Case","success");
  }  
  const handleLoClick = () => {
     //console.log('Uppercase was clicked ' + text);
     let newtext = text.toLowerCase();
     setText(newtext)
+    props.showAlert("Converted to Lower Case","success");
  }
  const handlerevClick = () => {
     //console.log('Uppercase was clicked ' + text);
@@ -25,21 +27,21 @@ const [text, setText] = useState('Enter Text Here');
 //setText('new text here'); Correct way to change the state
   return (
     <>
-    <div className="container">
+    <div className="container" style={{color: props.mode ==='dark'?'white':'black'}}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="10"></textarea>
+        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode ==='dark'?'grey':'white',color: props.mode ==='dark'?'white':'black'}} id="myBox" rows="10"></textarea>
         </div>
         <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
         <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowecase</button>
         <button className="btn btn-primary mx-2" onClick={handlerevClick}>Reverse the String</button>
     </div>
-    <div className="container my-3">
+    <div className="container my-3" style={{color: props.mode ==='dark'?'white':'black'}}>
         <h2>Your text Summery</h2>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{text.trim().length === 0 ? 0 : text.trim().split(/\s+/).length} words and {text.length} characters</p>
         <p>{ 0.008 * text.split(" ").length} Minutes Read</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length > 0?text:'Enter something into textbox above to preview here'}</p>
     </div>
     </>
   )
